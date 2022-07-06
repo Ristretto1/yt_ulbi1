44608f0613224dfe2a3ea084093042f0df5eb1bb
Antonov Kirill <antonovkir2@gmail.com>
2022-07-06 11:01:04 +0500

    add library

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
-import {UserList} from './components/UserList';
+import {Favorite} from './components/Favorite/Favorite';
+import {Gallery} from './components/Gallery/Gallery';
+import {Nav} from './components/Nav/Nav';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 export const App = () => {
     return (
-        <div>
-            <UserList/>
-        </div>
+        <BrowserRouter>
+            <Nav/>
+            <Routes>
+                <Route path="/" element={<Gallery/>}/>
+                <Route path="favorite" element={<Favorite/>}/>
+            </Routes>
+        </BrowserRouter>
     );
 };
 
